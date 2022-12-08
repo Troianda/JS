a6fa1e92a51c6ef97cd3ff40bedf990f2393bdbd
Vlad <dev.troian.horse@gmail.com>
2022-12-08 02:05:38 +0200

    first commit

---
+++ js/script71222.js
@@ -0,0 +1,56 @@
+// const arr = ["a", "b", "c"];
+// const arrObj = {
+//   a: "a",
+//   1: "b",
+//   2: "c"
+// };
+// const b = 'b';
+// // arrObj.b = '1234';
+// arrObj[b] = '1234';
+
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// const obj = {
+//   Anna: 500,
+//   Alice: 800
+// };
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Do you have 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What`s your name?', '');
+// answers[1] = prompt('What`s your sername?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+ 
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(25%8);
+
+    const isChecked = true,
+          isClose = true;
+
+          console.log(isChecked &&  isClose);
