b79ba23f97c0e0c2e7c9dd25b9c329376262a723
Vlad <dev.troian.horse@gmail.com>
2022-12-10 02:54:22 +0200

    CIKLS LES first

--- js/script71222.js
+++ js/script71222.js
@@ -172,25 +172,71 @@
 //     console.log('Мы уходим');
 // }
 
-const hamburger = 3; 
-const fries = 3;
+// const hamburger = 3; 
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// //ОПЕРАТОР ИЛИ (ЕСЛИ ХОТЬ ЧТО_ТО ЕСТЬ ОПЕРАЦИЯ ВЫПОЛНИТСЯ)
+// //КАК ТОЛЬКО "ИЛИ" ВИДИТ ПРАВДУ ОН ВОЗВРАЩАЕТ ЗНАЧЕНИЕ И ОСТАНАВЛИВАЕТСЯ 
+// //"ИЛИ(||) ЗАПИНАЕТСЯ НА ПРАВДЕ" 
+// //ЕСЛИ ВСЕ НЕ ПРАВДА ВОЗВР ПОСЛЕДНЕЕ ЛОЖНОЕ ЗНАЧЕНИЕ
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Все good!');
+// } else {
+//     console.log('Мы уходим');
+// }
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// console.log(!0); // ОПЕРАТОР РЕВЕРСА(ОТРИЦАНИЯ) ОБРАЩАЕТ ПРАВДУ В ЛОЖЬ
+
+// console.log( NaN || 2 || undefined );
+
+// console.log( NaN && 2 && undefined );
+
+
+// console.log( !1 && 2 || !3 ); // WHY FALSE?????
+
+// console.log( 25 || null && !3 );
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+let hamburger;
+const fries = NaN;
 const cola = 0;
 const nuggets = 2;
-
-//ОПЕРАТОР ИЛИ (ЕСЛИ ХОТЬ ЧТО_ТО ЕСТЬ ОПЕРАЦИЯ ВЫПОЛНИТСЯ)
-//КАК ТОЛЬКО "ИЛИ" ВИДИТ ПРАВДУ ОН ВОЗВРАЩАЕТ ЗНАЧЕНИЕ И ОСТАНАВЛИВАЕТСЯ 
-//"ИЛИ(||) ЗАПИНАЕТСЯ НА ПРАВДЕ" 
-//ЕСЛИ ВСЕ НЕ ПРАВДА ВОЗВР ПОСЛЕДНЕЕ ЛОЖНОЕ ЗНАЧЕНИЕ
-
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-    console.log('Все good!');
-} else {
-    console.log('Мы уходим');
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
 }
-console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
 
-let johnReport, alexReport, samReport, mariaReport = 'done';
 
-console.log(johnReport || alexReport || samReport || mariaReport);
 
-console.log(!0); // ОПЕРАТОР РЕВЕРСА ОБРАЩАЕТ ПРАВДУ В ЛОЖЬ
+
